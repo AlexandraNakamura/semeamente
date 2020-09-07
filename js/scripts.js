99fb3883a0cf045d718705d1c78dfc5531e25fc5
Nakka <designernakamura@gmail.com>
2020-09-07 21:47:37 +0000

    Atualização da LP e add imagem

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//INICIO DROPDOWN
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+//FIM DROPDOWN
+
+//INICIO TABS
+function abrirTela(evt, nomeTela) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(nomeTela).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+//FIM TABS
+
+
+
